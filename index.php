2b691c4892bfa334bb86768980373a5e4cdd0c3b
cristiaanx <125691592+cristiaanx@users.noreply.github.com>
2023-04-28 16:37:53 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; ?> 
+
+  
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
+<?php
+
+for ($i = 1; $i<=10; $i++){
+  echo $i . "<br>";
+}
+?>
